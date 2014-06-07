e839cacc7ea335872b32f7fb52139832bf845277
zhoujishi1993 <zhoujishi1993@gmail.com5>
2014-06-07 22:35:02 +0800

    This completes the JUnit test of Caculator

--- src/controller/Caculator.java
+++ src/controller/Caculator.java
@@ -6,19 +6,24 @@ package controller;
  * @version
  */
 public class Caculator {
-/**
- * @method  calCorrectness
- * @param reciteNum
- * @param correct
- * @return
- */
-public static double calCorrectness(int reciteNum,int correct){
-	   if(reciteNum == 0){
-		   throw new ArithmeticException("cannot have a reciteNum with 0");
-	   }else{
-		   int oneHundredCorrect = (int)((correct / reciteNum) * 100);
-		   return oneHundredCorrect / 100;
-	   }
-   }
-   
+	/**
+	 * @method calCorrectness
+	 * @param reciteNum
+	 * @param correct
+	 * @return
+	 */
+	public static double calCorrectness(int reciteNum, int correct) {
+		if (reciteNum == 0) {
+			return 0;
+		} else if (reciteNum < 0 || correct < 0) {
+			throw new ArithmeticException("cannot have negative parameters");
+		} else if (correct > reciteNum) {
+            throw new ArithmeticException("cannot have a correct numer larger than reciteNum");
+		} else {
+			// This will make the accuracy of 0.01
+			int oneHundredCorrect = (int) ((correct * 1.0 / reciteNum) * 100);
+			return oneHundredCorrect / 100.0;
+		}
+	}
+
 }
